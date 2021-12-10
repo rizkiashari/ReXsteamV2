94bfd1a715e777cb4928c8869e98ea053fbabb84
RizkiAshari <rizkiashari1026@gmail.com>
2021-12-11 00:55:44 +0700

    set cookies 2 hours

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cookie;
 
 class LoginController extends Controller
 {
@@ -23,24 +24,35 @@ class LoginController extends Controller
             'password' => ['required']
         ]);
 
-        $remember_me = (!empty($request->remember_me)) ? true : false;
 
         if (Auth::attempt($credentials)) {
+            $remember_me = (!empty($request->remember_me)) ? true : false;
             $request->session()->regenerate();
 
             $user = User::where(["username" => $credentials['username']])->first();
             Auth::login($user, $remember_me);
+            Cookie::queue(Auth::getRecallerName(), Cookie::get(Auth::getRecallerName()), 60 * 60 * 2);
+            if (Cookie::get(Auth::getRecallerName())) {
+                return redirect('/');
+            } else {
+                return redirect('/login');
+            }
             return redirect('/')->with('success', 'You are now logged in');
         }
 
         return back()->withErrors([
             'username' => 'Invalid user credentials.',
+            'password' => 'Invalid user credentials.'
         ]);
     }
 
     public function logout()
     {
-        Auth::logout();
-        return redirect('/');
+        Auth::logoutCurrentDevice();
+        request()->session()->invalidate();
+        Cookie::queue(Cookie::forget(Auth::getRecallerName()));
+        request()->session()->regenerateToken();
+
+        return redirect('/login');
     }
 }
