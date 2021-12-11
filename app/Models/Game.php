fab6709841e5af969af9478018d1d7b4652c57fe
RizkiAshari <rizkiashari1026@gmail.com>
2021-12-11 23:18:19 +0700

    add relation table

--- app/Models/Game.php
+++ app/Models/Game.php
@@ -15,4 +15,9 @@ class Game extends Model
     {
         return $this->belongsTo(Category::class, 'category_id');
     }
+
+    public function cartDetail()
+    {
+        return $this->hasOne(CartDetail::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -19,4 +19,9 @@ class User extends Authenticatable
     {
         return $this->belongsTo(Role::class);
     }
+
+    public function cart()
+    {
+        return $this->hasMany(Cart::class);
+    }
 }
